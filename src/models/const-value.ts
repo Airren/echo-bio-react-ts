82489b9d231067991f6193799daaa5aa56e91154
Airren <qiang.ren@intel.com>
2022-12-04 12:53:16 +0800

    💥 feat(compiler): update online call back

--- src/models/const-value.ts
+++ src/models/const-value.ts
@@ -1,5 +1,5 @@
 export const JwtToken = 'x-jwt-token';
-export const FileUploadPath = 'http://localhost:8080/api/v1/file/upload';
+export const FileUploadPath = 'http://www.echo-bio.cn:80/api/v1/file/upload';
 export const LoginPage =
-  'http://124.223.99.93:8001/login/oauth/authorize?client_id=aee24e6d160573b51f9d&' +
-  'response_type=code&redirect_uri=http://127.0.0.1:8000/user/login&scope=read&state=echo-bio-react';
+  'http://www.echo-bio.cn:8001/login/oauth/authorize?client_id=aee24e6d160573b51f9d&' +
+  'response_type=code&redirect_uri=http://www.echo-bio.cn:80/user/login&scope=read&state=echo-bio-react';
