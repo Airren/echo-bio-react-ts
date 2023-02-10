b55d8de577c4ecef233609eded408e5729cbcddd
Airren <qiang.ren@intel.com>
2023-02-11 00:23:31 +0800

    💥 feat: update deployed port

--- src/models/const-value.ts
+++ src/models/const-value.ts
@@ -2,7 +2,7 @@ export const JwtToken = 'x-jwt-token';
 export const FileUploadPath = '/api/v1/file/upload';
 export const LoginPage =
   'http://www.echo-bio.cn:8001/login/oauth/authorize?client_id=aee24e6d160573b51f9d&' +
-  'response_type=code&redirect_uri=http://www.echo-bio.cn:80/user/login&scope=read&state=echo-bio-react';
+  'response_type=code&redirect_uri=http://www.echo-bio.cn:8088/user/login&scope=read&state=echo-bio-react';
 // export const LoginPage =
 //   'http://www.echo-bio.cn:8001/login/oauth/authorize?client_id=aee24e6d160573b51f9d&' +
 //   'response_type=code&redirect_uri=http://127.0.0.1:8000/user/login&scope=read&state=echo-bio-react';
